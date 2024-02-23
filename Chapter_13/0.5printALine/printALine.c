f27c3c94cbc9f78fedf1a19163e5ec22f9b7b9d2
ma-jiale <majialea@126.com>
2024-02-23 21:17:39 +0800

    add 0.5printALine

---
+++ Chapter_13/0.5printALine/printALine.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define BUF 256
+int have_char(char ch, const char* line ,int size);
+int main(int argc, char* argv[])
+{
+    char ch;
+    FILE* fp;
+    long last;
+    char line[BUF];
+
+    if((fp = fopen(argv[2], "r")) == NULL)
+        {
+        puts("can't open the file.");
+        exit(EXIT_FAILURE);
+        }
+
+    while (fgets(line, BUF, fp) != NULL)
+    {
+        if(have_char(argv[1][0], line,BUF))
+            fputs(line, stdout);
+    }
+    
+    if(fclose(fp) != 0)
+        printf("Error in closing file\n");
+        exit(EXIT_FAILURE);
+    return 0;
+}
+
+int have_char(char ch, const char* line ,int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        if(line[i] == ch)
+            return 1;
+    }
+    return 0;
+}
